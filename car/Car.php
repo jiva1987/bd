81cf2e9b375ce2854b563e0947a7fd8333ff2b68
jiva1987 <klepalsky@gmail.com>
2017-09-14 18:52:14 +0300

    ddd

--- car/Car.php
+++ car/Car.php
@@ -2,8 +2,17 @@
 namespace car;
 use file\Foo;
 class Car{
+	public $b=1;
 	public function move(){
 		$a = new Foo;
 		$a->move();
 	}
+	static public function A(){
+		self::$a;
+	}
 }
+
+$a = 1;
+$a=(string)$a;
+var_dump(is_integer($a));
+echo "ffff{$a}ddds";
